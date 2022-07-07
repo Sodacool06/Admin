a5a16c68462a75548f3a0c49b6b973a9d39599b2
Sodacool06 <96247666+Sodacool06@users.noreply.github.com>
2022-07-06 19:50:41 -0500

    Create main.lua

---
+++ main.lua
@@ -0,0 +1,5 @@
+local Message = loadstring(game:HttpGet("https://raw.githubusercontent.com/frel0/FakeKickLibrary/main/FakeKickLibrary.lua", true))()
+
+Message:Kick("You have been kicked from this experience: Anti-explots: Banned Perminatly")
+wait(3)
+Message:RickRoll()
